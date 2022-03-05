730d0df4b22dcbb5ecf479d311abd8fc28f7abbb
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-04 17:58:39 -0800

    [add] deps and components

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   return (
@@ -7,6 +9,12 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    //   <NavigationContainer>
+    //   <Tab.Navigator>
+    //     <Tab.Screen name="Home" component={HomeScreen} />
+    //     <Tab.Screen name="Settings" component={SettingsScreen} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
   );
 }
 
---
+++ components/Bands.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Bands = () => {
+  return (
+    <View>
+      <Text>Bands</Text>
+    </View>
+  )
+}
+
+export default Bands
+
+const styles = StyleSheet.create({})
